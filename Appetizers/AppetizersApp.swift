1c643aaf9dbc02eb5aa1c13acf8c581c2fdf4389
Sahas Pasya <pasyasahas17@gmail.com>
2025-04-02 14:16:47 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Pasya Sahas on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
